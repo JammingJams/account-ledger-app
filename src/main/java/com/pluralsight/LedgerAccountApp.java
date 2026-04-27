71d14ea10bb23b8dd959550a9c8d6d315d4571cd
JammingJams <noahlogan20o00@gmail.com>
2026-04-27 10:39:23 -0400

    Added parts to the file writer! IE setting new object datatypes and catching any user misinput!

--- src/main/java/com/pluralsight/LedgerAccountApp.java
+++ src/main/java/com/pluralsight/LedgerAccountApp.java
@@ -1,15 +1,36 @@
 package com.pluralsight;
 
-import java.util.ArrayList;
+import javax.swing.tree.TreeCellRenderer;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class LedgerApp {
+public class LedgerAccountApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         boolean userInHomescreen = true;
         boolean userInLedger = true;
         String homeScreenOption = "";
+        String userSelection = "";
+        double priceSelection = 0;
+        HashMap<String, Transaction> depositTransactionHashMap = new HashMap<>();
+        HashMap<String, Transaction> paymentTransactionHashMap = new HashMap<>();
+
+        DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern(("hh:mm:ss"));
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        /*Transaction obj = new Transaction();
+        obj.setPrice(90.00);
+        transactionHashMap.put("Hello world", obj);*/
+
+
 
         System.out.println("Welcome to the account ledger");
         System.out.println("(D) Add deposit\n(P) Make Payment (Debit)\n(L) Ledger\n(X) exit");
@@ -17,25 +38,71 @@ public class LedgerApp {
 
 
         while (userInHomescreen) {
+            userInLedger = true;
+            userSelection = "";
             homeScreenOption = sc.nextLine().trim().toLowerCase();
 
             switch (homeScreenOption) {
 
-                case ("d") -> {}
+                case ("d") -> {
+                    try {
+                        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("src/main/resources/transactions.csv",true));
+                        Transaction depositTransaction = new Transaction();
+                        LocalTime userCurrentTime = LocalTime.now().withSecond(2).withNano(0);
+                        LocalDate userCurrentDate = LocalDate.now();
+
+                        //This area sets the new objects datatypes
+                        depositTransaction.setTransactionDate(userCurrentDate);
+                        depositTransaction.setTransactionTime(userCurrentTime);
+                        System.out.print("Put in what you are paying for: ");
+                        depositTransaction.setTransactionDescription(userSelection = sc.nextLine());
+                        System.out.println("Put in what who you are buying from: ");
+                        depositTransaction.setTransactionDescription(userSelection = sc.nextLine());
+
+                        //this catches any user misinputs
+                        while (true) {
+                            try {
+                                depositTransaction.setPrice(priceSelection = sc.nextDouble());
+                                sc.nextLine();
+                                break;
+                            } catch (InputMismatchException e) {
+                                //e.printStackTrace();
+                                System.out.println("Hey please input a valid number not a string!: ");
+                                sc.nextLine();
+                                priceSelection = sc.nextDouble();
+                            }
+                        }
+
+
+
+
+                    }
+                    catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
                 case ("p") -> {}
                 case ("l") -> {
                     while (userInLedger) {
-                        System.out.println("(A)");
+                        System.out.println("(A) Display all entries\n(D) Display all entries that are deposits\n(P) Display negative entries\n(R) Run pre-defined reports\n(H) Go back to homepage");
+                        System.out.print("Type your selection here: ");
+                        userSelection = sc.nextLine().trim().toLowerCase();
 
+                        switch (userSelection) {
+                            case ("a") -> {}
+                            case ("d") -> {}
+                            case ("p") -> {}
+                            case ("r") -> {}
+                            case ("h") -> {userInLedger = false;}
+                            default -> {System.out.println("Invalid user input");}
+                        }
                     }
                 }
                 case ("x") -> {userInHomescreen = false;}
                 default -> {System.out.println("Incorrect user input");}
 
             }
-
-
-
 
 
 
--- src/main/java/com/pluralsight/Transaction.java
+++ src/main/java/com/pluralsight/Transaction.java
@@ -1,4 +1,36 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Transaction {
+    private LocalDate transactionDate;
+    private LocalTime transactionTime;
+    private String transactionDescription;
+    private String vendor;
+    private Double price;
+
+    public Transaction() {
+        this.transactionDate = transactionDate;
+        this.transactionTime = transactionTime;
+        this.transactionDescription = transactionDescription;
+        this.vendor = vendor;
+        this.price = price;
+    }
+
+    //These are the getters
+    public LocalDate getTransactionDate() {return transactionDate;}
+    public LocalTime getTransactionTime() {return transactionTime;}
+    public String getTransactionDescription() {return transactionDescription;}
+    public String getVendor() {return vendor;}
+    public Double getPrice() {return price;}
+
+    //These are the setters
+    public void setTransactionDate(LocalDate transactionDate) {this.transactionDate = transactionDate;}
+    public void setTransactionTime(LocalTime transactionTime) {this.transactionTime = transactionTime;}
+    public void setTransactionDescription(String transactionDescription) {this.transactionDescription = transactionDescription;}
+    public void setVendor(String vendor) {this.vendor = vendor;}
+    public void setPrice(Double price) {this.price = price;}
+
+
 }
